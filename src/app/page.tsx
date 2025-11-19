d5839b04a07e139fc390bd946a6ffb8450c0211c
s1f102302988 <s1f102302988@iniad.org>
2025-11-19 16:08:13 +0900

    修正2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { HeroSection } from "../components/HeroSection/HeroSection";
 import { FeaturesSection } from "../components/FeaturesSection/FeaturesSection";
 import { JobListings } from "../components/JobListings/JobListings";
-import { LifeSimulator } from "../components/LifeSimulator/LifeSimulator";
 import { RegionSpotlight } from "../components/RegionSpotlight/RegionSpotlight";
 import { Footer } from "../components/Footer/Footer";
 
