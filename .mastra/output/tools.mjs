66a2e2d7e66a75646944b570915835ffb290a3d5
s1F10230108 <s1F102301088@iniad.org>
2025-12-17 15:49:31 +0900

    chore: .mastraをgit管理から排除

--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/2537e3ba-dd27-4c9c-b477-6a9931a90b4b.mjs';
-import * as tool1 from './tools/47c9512b-fa4b-4073-bd3b-b7c86bb5bb4d.mjs';
-        
-                export const tools = [tool0, tool1]
